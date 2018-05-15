d995b46fc9d725dfa031e428ae1ea828e96e3c33
jssss93 <cjs0533@naver.com>
2018-05-15 14:33:33 +0900

    20180515 14:30 -WriteACtion.java/boardWrite.jsp

---
+++ struts_board/src/board/WriteAction.java
@@ -0,0 +1,198 @@
+package board;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class WriteAction extends ActionSupport {
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+
+	private boardVO paramClass;
+	private boardVO resultClass;
+
+	private int currentPage;
+
+	private int no;
+	private String subject;
+	private String name;
+	private String password;
+	private String content;
+	private String file_orgName;
+	private String file_savName;
+
+	Calendar today = Calendar.getInstance();
+
+	private File upload;
+	private String uploadContentType;
+	private String uploadFileName;
+	private String fileUploadPath = "C:\\upload\\";
+
+	public WriteAction() throws IOException {
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+
+	public String form() throws Exception {
+		return SUCCESS;
+	}
+
+	public String execute() throws Exception {
+		paramClass = new boardVO();
+		resultClass = new boardVO();
+
+		paramClass.setSubject(getSubject());
+		paramClass.setName(getName());
+		paramClass.setPassword(getPassword());
+		paramClass.setContent(getContent());
+		paramClass.setRegdate(today.getTime());
+		
+		sqlMapper.insert("insertBoard", paramClass);
+		
+		if(getUpload()!=null) {
+			resultClass=(boardVO) sqlMapper.queryForObject("selectLastNo");
+			String file_name ="file_"+ resultClass.getNo();
+			String file_ext=getUploadFileName().substring(getUploadFileName().lastIndexOf('.')+1,
+					getUploadFileName().length());
+			
+			File destFile=new File(fileUploadPath+file_name+"."+file_ext);
+			FileUtils.copyFile(getUpload(), destFile);
+			
+			paramClass.setNo(resultClass.getNo());
+			paramClass.setFile_orgname(getUploadFileName());
+			paramClass.setFile_savname(file_name+"."+file_ext);
+			
+			sqlMapper.update("updateFile",paramClass);
+		}
+		return SUCCESS;
+	}
+
+	public Calendar getToday() {
+		return today;
+	}
+
+	public void setToday(Calendar today) {
+		this.today = today;
+	}
+
+	public boardVO getParamClass() {
+		return paramClass;
+	}
+
+	public void setParamClass(boardVO paramClass) {
+		this.paramClass = paramClass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public File getUpload() {
+		return upload;
+	}
+
+	public void setUpload(File upload) {
+		this.upload = upload;
+	}
+
+	public String getUploadContentType() {
+		return uploadContentType;
+	}
+
+	public void setUploadContentType(String uploadContentType) {
+		this.uploadContentType = uploadContentType;
+	}
+
+	public String getUploadFileName() {
+		return uploadFileName;
+	}
+
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+
+	public String getFileUploadPath() {
+		return fileUploadPath;
+	}
+
+	public void setFileUploadPath(String fileUploadPath) {
+		this.fileUploadPath = fileUploadPath;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getFile_orgName() {
+		return file_orgName;
+	}
+
+	public void setFile_orgName(String file_orgName) {
+		this.file_orgName = file_orgName;
+	}
+
+	public String getFile_savName() {
+		return file_savName;
+	}
+
+	public void setFile_savName(String file_savName) {
+		this.file_savName = file_savName;
+	}
+
+	public boardVO getResultClass() {
+		return resultClass;
+	}
+
+	public void setResultClass(boardVO resultClass) {
+		this.resultClass = resultClass;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+}
