729fcff4a70da15bb7c44fb256dff893ac9cebda
jssss93 <cjs0533@naver.com>
2018-05-16 13:39:04 +0900

    -20180516 13:38
    modifyAction.java 작성

---
+++ struts_board/src/board/modifyAction.java
@@ -0,0 +1,188 @@
+package board;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.io.FileUtils;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class modifyAction extends ActionSupport {
+
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+
+	private boardVO paramClass;
+	private boardVO resultClass;
+
+	private int currentPage;
+
+	private int no;
+	private String subject;
+	private String name;
+	private String password;
+	private String content;
+	private String old_file;
+
+	private File upload; // 파일 객체
+	private String uploadContentType; // 컨텐츠 타입
+	private String uploadFileName; // 파일 이름
+	private String fileUploadPath = "C:\\upload\\"; // 업로드 경로.
+
+	// 생성자
+	public modifyAction() throws IOException {
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+
+	// 수정들어간다
+	public String execute() throws Exception {
+
+		paramClass = new boardVO();
+		resultClass = new boardVO();
+
+		paramClass.setNo(getNo());
+		paramClass.setSubject(getSubject());
+		paramClass.setName(getName());
+		paramClass.setPassword(getPassword());
+		paramClass.setContent(getContent());
+		
+		sqlMapper.update("updateBoard",paramClass);
+		
+		if(getUpload()!=null) {
+			String file_name = "file_"+getNo();
+			String file_ext=getUploadFileName().substring(getUploadFileName().lastIndexOf('.')+1,getUploadFileName().length());
+			
+			//이전파일 삭제
+			File deleteFile=new File(fileUploadPath+getOld_file());
+			deleteFile.delete();
+			
+			//새 파일 업로드
+			File destFile=new File(fileUploadPath+file_name+"."+file_ext);
+			FileUtils.copyFile(getUpload(), destFile);
+			
+			//파일 정보 파라미터 설정?
+			paramClass.setFile_orgname(getUploadFileName());
+			paramClass.setFile_savname(file_name+"."+file_ext);
+			
+			//파일 정보 업데이트
+			sqlMapper.update("updateFile",paramClass);
+		}
+		
+		//수정이 끝나면 view 로 이동
+		resultClass=(boardVO)sqlMapper.queryForObject("selectOne",getNo());
+			
+		return SUCCESS;
+	}
+
+
+
+	public boardVO getParamClass() {
+		return paramClass;
+	}
+
+	public void setParamClass(boardVO paramClass) {
+		this.paramClass = paramClass;
+	}
+
+	public boardVO getResultClass() {
+		return resultClass;
+	}
+
+	public void setResultClass(boardVO resultClass) {
+		this.resultClass = resultClass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public File getUpload() {
+		return upload;
+	}
+
+	public void setUpload(File upload) {
+		this.upload = upload;
+	}
+
+	public String getUploadContentType() {
+		return uploadContentType;
+	}
+
+	public void setUploadContentType(String uploadContentType) {
+		this.uploadContentType = uploadContentType;
+	}
+
+	public String getUploadFileName() {
+		return uploadFileName;
+	}
+
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+
+	public String getFileUploadPath() {
+		return fileUploadPath;
+	}
+
+	public void setFileUploadPath(String fileUploadPath) {
+		this.fileUploadPath = fileUploadPath;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getOld_file() {
+		return old_file;
+	}
+
+	public void setOld_file(String old_file) {
+		this.old_file = old_file;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+}
